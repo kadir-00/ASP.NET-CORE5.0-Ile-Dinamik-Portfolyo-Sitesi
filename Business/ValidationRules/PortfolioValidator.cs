9e54225afc3a94c596481a297b55c1dc2969cf13
kadir-00 <20215802023@ogr.akdeniz.edu.tr>
2024-06-20 16:29:42 +0300

    Fluent Validations eklendi , Portfolio kismi CRUD islemleri yapildi

---
+++ Business/ValidationRules/PortfolioValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace Business.ValidationRules
+{
+    public class PortfolioValidator : AbstractValidator<Portfolio>
+    {
+        public PortfolioValidator()
+        {
+                RuleFor(x => x.Name).NotEmpty().WithMessage("Proje adi bos gecilemez");
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Gorsel Alani Bos Gecilemez");
+            RuleFor(x => x.ImageUrl2).NotEmpty().WithMessage("Gorsel Alani Bos Gecilemez");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat Alani Bos Gecilemez");
+            RuleFor(x => x.Value).NotEmpty().WithMessage("Deger Alani Bos Gecilemez");
+            RuleFor(x => x.Name).MinimumLength(5).WithMessage("EN AZ 5 Karakterden olusmali ");
+        }
+    }
+}
---
+++ Core2/Controllers/PortfolioController.cs
@@ -0,0 +1,90 @@
+﻿using Business.Concrete;
+using Business.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using FluentValidation;
+
+namespace Core2.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio p)
+        {
+            
+
+            PortfolioValidator validatons = new PortfolioValidator();
+            FluentValidation.Results.ValidationResult validationResult = validatons.Validate(p);
+           
+            if (validationResult.IsValid)
+            {
+                portfolioManager.TAdd(p);
+                return RedirectToAction("Index");
+            }
+            else 
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName , item.ErrorMessage);
+                }
+            }
+            return View();
+           
+
+        }
+
+        public IActionResult DeletePortfolio(int id)
+        {
+            var values = portfolioManager.TGetById(id);
+            portfolioManager.TDelete(values);
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult EditPortfolio(int id)
+        {
+           
+            var values = portfolioManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult EditPortfolio(Portfolio portfolio)
+        {
+            PortfolioValidator validatons = new PortfolioValidator();
+            FluentValidation.Results.ValidationResult validationResult = validatons.Validate(portfolio);
+            if (validationResult.IsValid)
+            {
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName , item.ErrorMessage);
+                }
+            }
+            return View();
+          
+            
+        }
+    }
+}
